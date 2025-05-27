d5ef5bed66e1b64fc5c201e020c5923a95b876be
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-05-27 12:46:47 +0600

    feat: add API response type definitions for Tag and Resource

---
+++ src/types/api.ts
@@ -0,0 +1,13 @@
+export interface Tag{
+    tag: string;
+    id: string;
+}
+
+export interface Resources{
+    author: string;
+    name: string;
+    appliedTags: string[];
+    url:string;
+    createdAt: string;
+    id: string;
+}
