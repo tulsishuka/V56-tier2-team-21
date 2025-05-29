11da1a8a27d1712ad7f741bbde530bfb8edf3681
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-05-30 00:20:46 +0600

    refactor: removed search button from ui

--- src/features/search/components/SearchBar.tsx
+++ src/features/search/components/SearchBar.tsx
@@ -5,15 +5,17 @@ const SearchBar: React.FC<SearchBarProps> = ({
   searchTerm,
   onSearchTermChange,
   onSubmit,
-  isLoading = false
+  isLoading = false,
 }) => {
-
-  const handleSubmit=(e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit();
-  }
+  };
   return (
-    <form onSubmit={handleSubmit} className="flex gap-2 w-full items-center mx-auto flex-col md:flex-row">
+    <form
+      onSubmit={handleSubmit}
+      className="flex gap-2 w-full items-center mx-auto flex-col md:flex-row"
+    >
       <input
         type="text"
         placeholder="Search..."
@@ -21,9 +23,6 @@ const SearchBar: React.FC<SearchBarProps> = ({
         onChange={(e) => onSearchTermChange(e.target.value)}
         className="border border-gray rounded-md px-4 py-2 text-gray-700 flex flex-grow w-9/10"
       />
-      <button type="submit" disabled={isLoading} onClick={onSubmit} className="px-6 py-2 bg-blue-500 hover:bg-blue-700 disabled:bg-blue-300 text-white rounded-lg transition-colors">
-        {isLoading ? 'Searching' : 'Search'}
-      </button>
     </form>
   );
 };
