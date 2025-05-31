f0aaf486333724e4ff83d1166cf1a548f1065eae
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-05-31 20:47:59 +0600

    ui: add search icon

--- src/features/search/components/SearchBar.tsx
+++ src/features/search/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import type { SearchBarProps } from "@/types/api";
 import React from "react";
-
+import { FcSearch } from "react-icons/fc";
 const SearchBar: React.FC<SearchBarProps> = ({
   searchTerm,
   onSearchTermChange,
@@ -16,13 +16,18 @@ const SearchBar: React.FC<SearchBarProps> = ({
       onSubmit={handleSubmit}
       className="flex gap-2 w-full items-center mx-auto flex-col md:flex-row"
     >
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => onSearchTermChange(e.target.value)}
-        className="border border-gray rounded-md px-4 py-2 text-gray-700 flex flex-grow w-9/10"
-      />
+      <div className="relative flex flex-grow w-9/10">
+        <span className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FcSearch />
+        </span>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => onSearchTermChange(e.target.value)}
+          className="border border-gray rounded-md px-4 py-2 text-gray-700 pl-10 w-full"
+        />
+      </div>
     </form>
   );
 };
