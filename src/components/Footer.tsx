c7c9308d59af53985b4330d987c4b8e0e699bc2b
elva <jia_shen@126.com>
2025-06-01 14:07:03 +0800

    feat: update github link in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <span>Created by</span>
         <a href='https://github.com/elva329' target="_blank" rel="noopener noreferrer">@elva329</a>
         <a href='https://github.com/rajin-siam' target="_blank" rel="noopener noreferrer">@rajin-siam</a>
-        <a href='https://github.com/albert' target="_blank" rel="noopener noreferrer">@Albert</a>
+        <a href='https://github.com/allenmathews' target="_blank" rel="noopener noreferrer">@Albert</a>
       </div>
       <div>
         <a href='https://github.com/chingu-voyages/V55-tier2-team-21' target="_blank" rel="noopener noreferrer">GitHub</a>
