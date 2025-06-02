f6d73ab8739da472b29da35feb1fef04b56d3917
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-06-02 12:05:06 +0600

    feat: integrate searching feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Chat from './components/Chat'
 import Footer from './components/Footer'
+import { SearchBarAndTagContainer } from './features/search/containers/SearchBarAndTagContainer'
 
 function App() {
   return (
     <div className='flex flex-col h-screen justify-between'>
+      <SearchBarAndTagContainer/>
       <Footer />
       <Chat />
     </div>
