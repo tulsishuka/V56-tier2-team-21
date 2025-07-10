46e8d9e44d103f17458d08c176d48cdbadb00666
elva <jia_shen@126.com>
2025-07-10 15:54:09 +0800

    feat: implemented the homepage UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Container from './features/container/Container'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Container />
       <Footer />
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center mt-5'>
+    <div className='flex items-center justify-center mt-5 gap-10'>
       <img src='/surgery-logo.png' className='' />
-      <div className='font-bold text-2xl'>Surgery Status Board</div>
+      <div className='font-bold text-4xl'>Surgery Status Board</div>
     </div>
   )
 }
