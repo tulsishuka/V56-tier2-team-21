f69552f8da30f6d36f775f7b09c7dca0a66e4b55
elva <jia_shen@126.com>
2025-07-10 11:18:36 +0800

    feat: updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+
 const Header = () => {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center justify-center mt-5'>
+      <img src='/surgery-logo.png' className='' />
+      <div className='font-bold text-2xl'>Surgery Status Board</div>
     </div>
   )
 }
