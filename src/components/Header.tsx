1d3ba6e7607ce99f30a6cf19dbe01abff2650ad2
elva <jia_shen@126.com>
2025-07-11 17:58:50 +0800

    feat: added home redirect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
-    <div className='flex items-center justify-center mt-5 gap-10'>
-      <img src='/surgery-logo.png' className='' />
-      <div className='font-bold text-4xl'>Surgery Status Board</div>
+    <div className="relative flex items-center justify-center mt-10 h-16">
+      {/* Home icon on the left */}
+      {currentPath !== "/" &&
+        <div className="absolute left-10 flex items-center justify-center gap-2 cursor-pointer font-bold" onClick={() => navigate('/')}>
+          <img src="/home.png" className=" size-12 ml-4" />
+          <span>HOME</span>
+        </div>}
+
+      {/* Centered logo and title */}
+      <div className="flex items-center gap-4">
+        <img src="/surgery-logo.png" />
+        <div className="font-bold text-4xl">Surgery Status Board</div>
+      </div>
     </div>
   )
 }
