a2260ba16fa04237012849a820a41b9239e604e0
elva <jia_shen@126.com>
2025-07-11 16:32:01 +0800

    feat: added login form

--- src/features/container/Container.tsx
+++ src/features/container/Container.tsx
@@ -1,16 +1,24 @@
 
+import { useNavigate } from 'react-router-dom';
+
 const Container = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex justify-center items-center gap-10 h-[50vh]'>
       <div className='flex flex-col items-center justify-between w-52 h-52 border p-2 rounded-2xl cursor-pointer'>
         <img src='/guest.svg' className='w-full h-4/5 object-cover' />
         <span className='text-xl font-bold'>Guest</span>
       </div>
-      <div className='flex flex-col items-center justify-between w-52 h-52 border p-2 rounded-2xl cursor-pointer'>
+      <div
+        className='flex flex-col items-center justify-between 
+                    w-52 h-52 border p-2 rounded-2xl 
+                    cursor-pointer hover:shadow-lg transition'
+        onClick={() => navigate('/login')}
+      >
         <img src='/admin.png' className='w-4/5 h-auto object-cover opacity-50' />
         <span className='text-xl font-bold'>Admin</span>
       </div>
-      <div className='flex flex-col items-center justify-between w-52 h-52 border pb-2 rounded-2xl cursor-pointer'>
+      <div className='flex flex-col items-center justify-between w-52 h-52 border pb-2 rounded-2xl cursor-pointer hover:shadow-lg transition'>
         <img src='/surgery-team.png' className='w-10/12 h-auto object-cover' />
         <span className='text-xl font-bold'>Surgery Team</span>
       </div>
