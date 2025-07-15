e3428adab9b3ed07c556ac23e039b9f08e2b2b6a
Tulasi <tulsishuklag@gmail.com>
2025-07-15 16:27:20 +0530

    "Modified footer page"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
 
+import { useState, useEffect } from "react";
 const Header = () => {
-  return (
-    <div className='flex items-center justify-center mt-5 gap-10'>
-      <img src='/surgery-logo.png' className='' />
-      <div className='font-bold text-4xl'>Surgery Status Board</div>
-    </div>
-  )
-}
+  const [currentDate, setCurrentDate] = useState<string>("");
 
-export default Header
+ useEffect(() => {
+    const today = new Date();
+    const dateString = today.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    setCurrentDate(dateString);
+  }, []);
+
+
+  return (
+   
+    <div className="fixed top-0 w-full bg-gradient-to-b from-[#0a1e4d] to-[#1e0a8a] text-white py-3 px-4 shadow-inner flex items-center justify-between">
+      <div className="flex">
+        <img src="/surgery-logo.png" className="w-7" />
+        <h1 className="text-2xl font-bold  ml-2">Surgery Status Board</h1>
+      </div>
+
+      <nav>
+        <ul className="flex space-x-4 ">
+          <li>Patient info</li>
+          <li>Surgeon info</li>
+          <li>Update</li>
+        </ul>
+      </nav>
+       <div className=" ml-4">{currentDate}</div>
+    </div>
+    // </header>
+   
+  );
+};
+
+   export default Header
