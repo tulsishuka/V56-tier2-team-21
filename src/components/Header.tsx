a152496e7472932374b5010e2ae1747e59e1a66d
elva <jia_shen@126.com>
2025-07-15 16:50:08 +0800

    feat: updated login-form style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,13 @@ const Header = () => {
     <div className="relative flex items-center justify-center mt-10 h-16">
       {/* Home icon on the left */}
       {currentPath !== "/" &&
-        <div className="absolute left-10 flex items-center justify-center gap-2 cursor-pointer font-bold" onClick={() => navigate('/')}>
+        <div className="absolute left-0 flex items-center justify-center gap-2 cursor-pointer font-bold" onClick={() => navigate('/')}>
           <img src="/home.png" className=" size-12 ml-4" />
           <span>HOME</span>
         </div>}
 
-      {/* Centered logo and title */}
       <div className="flex items-center gap-4">
-        <img src="/surgery-logo.png" />
-        <div className="font-bold text-4xl">Surgery Status Board</div>
+        <div className="font-bold text-2xl">Staff Login (Admin / Surgical Team)</div>
       </div>
     </div>
   )
