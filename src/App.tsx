89e12a0c5c368bd481b485f82ac420b3abba397f
Tulasi <tulsishuklag@gmail.com>
2025-07-16 16:41:58 +0530

    change  src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+// import Footer from './components/Footer'
+// import Container from './features/container/Container'
+// import Login from './features/login/Login'
+
+// function App() {
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Container />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+
+//       <Footer />
+//     </Router>
+//   )
+// }
+
+// export default App
 import Footer from './components/Footer'
+import Header from './components/Header'
 import Container from './features/container/Container'
-import Login from './features/login/Login'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Container />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-
+    <>
+      <Header />
+      <Container />
       <Footer />
-    </Router>
+    </>
   )
 }
 
