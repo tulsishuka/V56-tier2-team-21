9c8dc3cec259a988984db0cb67fddf8955a47ebe
elva <jia_shen@126.com>
2025-07-17 20:10:56 +0800

    feat: added admin and guest router

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,40 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Container from './features/container/Container'
 import Login from './features/login/Login'
+import { AuthProvider } from './features/auth/AuthContext'
+import PrivateRoute from './features/auth/PrivateRoute'
 
 function App() {
+  const AdminPage = () => <div>Admin Page (Protected)</div>;
+  const GuestPage = () => <div>Guest Page (Protected)</div>;
+
   return (
-    <Router>
-      <Routes>
-        <Route />
-        <Route path="/" element={<Container />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route />
+          <Route path="/" element={<Container />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/guest"
+            element={
+              <PrivateRoute>
+                <GuestPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
   )
 }
 
